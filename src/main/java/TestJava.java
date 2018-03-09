8f04967cd9fc6d1441df4127bfc482f78f52f632
zhouMac <LazyZhou1997@gmail.com>
2018-03-09 13:55:26 +0800

    测试Java中继承时子类调用父类构造方法

--- src/main/java/TestJava.java
+++ src/main/java/TestJava.java
@@ -2,20 +2,23 @@ public class TestJava {
 
     public static void main(String[] args) {
 
-        String a = "a";
-        switch (a) {
-            case "":
-                System.out.println("");
-                break;
-            case "a":
-                System.out.println("a");
-                break;
-            case "b":
-                System.out.println("b");
-                break;
-            default:
-                System.out.println("Other");
-                break;
-        }
+        Child child = new Child();
+        child.doSomething();
+    }
+
+}
+
+class Parent{
+    private Parent(){
+
+    }
+
+    
+}
+
+class Child extends Parent{
+
+    public void doSomething(){
+        System.out.println("In Child!");
     }
 }
