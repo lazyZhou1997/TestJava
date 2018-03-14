ad464d607b962fb56fa3314d9d4b563f378a3cfc
zhouMac <LazyZhou1997@gmail.com>
2018-03-14 16:30:17 +0800

    测试Java抽象类中抽象方法的可以使用的修饰符

--- src/main/java/edu/scu/TestJava.java
+++ src/main/java/edu/scu/TestJava.java
@@ -1,15 +1,29 @@
 package edu.scu;
 
 /**
- * 测试Java的基本类型运算时的自动类型转换
+ * 测试Java抽象类中抽象方法的可以使用的修饰符
  */
 public class TestJava {
-    public static void main(String[] args) {
 
-        byte a1 = 21, a2 = 4, a3;
-        short s = 16;
-        a2 = s;
-        a3 = a1 * a2;
+    public static void main(String[] args) {
 
     }
 }
+
+abstract class TestAbstractMethod{
+
+    //抽象方法不能使用private
+    private abstract void testPrivate();
+
+    //非抽象方法可以是private
+    private void testPrivate(int a){
+
+    }
+
+    //protected可以是抽象的
+    protected abstract void testProtected();
+
+
+
+}
+
