f253ad5d37d77af1ad26e183a46d6caf66b309f1
zhouMac <LazyZhou1997@gmail.com>
2018-03-15 22:42:28 +0800

    学习受类型限制的泛型

--- src/main/java/edu/scu/TestJava.java
+++ src/main/java/edu/scu/TestJava.java
@@ -2,46 +2,35 @@ package edu.scu;
 
 
 /**
- * 学习泛型，受类型限制的泛型方法
+ * 学习泛型，受限制的类泛型
  */
 public class TestJava {
 
     public static void main(String[] args) {
 
-        TestJava testJava = new TestJava();
+        Box<Integer> integerBox = new Box<Integer>();
+        Box<String> stringBox = new Box<String>();
+        //没有实现Comparable接口的类不能使用
+        //Box<TestJava> testJavaBox = new Box<TestJava>();
 
-        System.out.printf( "Max of %d, %d and %d is %d\n\n",
-                3, 4, 5, testJava.maximum( 3, 4, 5 ) );
+        integerBox.add(new Integer(10));
+        stringBox.add(new String("Hello World"));
 
-        System.out.printf( "Maxm of %.1f,%.1f and %.1f is %.1f\n\n",
-                6.6, 8.8, 7.7, testJava.maximum( 6.6, 8.8, 7.7 ) );
+        System.out.printf("Integer Value :%d\n\n", integerBox.get());
+        System.out.printf("String Value :%s\n", stringBox.get());
+    }
+}
 
-        System.out.printf( "Max of %s, %s and %s is %s\n","pear",
-                "apple", "orange", testJava.maximum( "pear", "apple", "orange" ) );
+class Box<T extends Comparable<T>>{
 
+    private T t;
+
+    public void add(T t){
+        this.t = t;
     }
 
-    /**
-     * 求最大值
-     * @param x
-     * @param y
-     * @param z
-     * @param <T>
-     * @return
-     */
-    public <T extends Comparable<T>> T maximum(T x, T y,T z){
-        //假定x是最大值
-        T max = x;
-        //先比较max和y
-        if (max.compareTo(y)>0){
-            max = y;
-        }
-        //再比较max与z
-        if (max.compareTo(z)>0){
-            max = z;
-        }
-
-        return max;
+    public T get(){
+        return t;
     }
 
 }
